fdd8b6ba47a7471af1ae1f365c7943e39b8c1bc0
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-12 09:56:49 +0300

    Init project

---
+++ DotNetCore_AutoLotDAL/DotNetCore_AutoLotDAL.TextDriver/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetCore_AutoLotDAL.TextDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}

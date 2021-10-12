ac0b1f34668587da0b16074d1ea83a1b420ae744
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-12 20:46:18 +0300

    Add DbContext

---
+++ DotNetCore_AutoLotDAL/DotNetCore_AutoLotDAL/EF/AutoLotContext.cs
@@ -0,0 +1,53 @@
+﻿using DotNetCore_AutoLotDAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore_AutoLotDAL.EF
+{
+    public class AutoLotContext: DbContext
+    {
+        public DbSet<CreditRisk> CreditRisks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Inventory> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        internal AutoLotContext()
+        {
+        }
+
+        public AutoLotContext(DbContextOptions options): base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = @"server=.; database=AutoLotCore2; integrated security=True; MultipleActiveResultSets=True; App=EntityFramework";
+                optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure())
+                    .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CreditRisk>(entity =>
+            {
+                entity.HasIndex(e => new { e.FirstName, e.LastName }).IsUnique();
+            });
+
+            modelBuilder.Entity<Order>()
+                .HasOne(e => e.Car)
+                .WithMany(e => e.Orders)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+
+        public string GetTableName(Type type)
+        {
+            return Model.FindEntityType(type).SqlServer().TableName;
+        }
+    }
+}
---
+++ DotNetCore_AutoLotDAL/DotNetCore_AutoLotDAL/EF/AutoLotContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore_AutoLotDAL.EF
+{
+    public class AutoLotContextFactory : IDesignTimeDbContextFactory<AutoLotContext>
+    {
+        public AutoLotContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AutoLotContext>();
+
+            var connectionString = @"server=.; database=AutoLotCore2; integrated security=True; MultipleActiveResultSets=True; App=EntityFramework";
+            optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new AutoLotContext(optionsBuilder.Options);
+        }
+    }
+}

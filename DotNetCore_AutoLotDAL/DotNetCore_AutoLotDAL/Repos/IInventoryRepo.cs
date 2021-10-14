124112272eaee451a50303dbd3b0aea3ed4d7454
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-14 10:14:25 +0300

    Add Inventory Repo

---
+++ DotNetCore_AutoLotDAL/DotNetCore_AutoLotDAL/Repos/IInventoryRepo.cs
@@ -0,0 +1,16 @@
+﻿using DotNetCore_AutoLotDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore_AutoLotDAL.Repos
+{
+    public interface IInventoryRepo: IRepo<Inventory>
+    {
+        List<Inventory> Search(string searchString);
+
+        List<Inventory> GetPinkCars();
+
+        List<Inventory> GetRelatedData();
+    }
+}
---
+++ DotNetCore_AutoLotDAL/DotNetCore_AutoLotDAL/Repos/InventoryRepo.cs
@@ -0,0 +1,27 @@
+﻿using DotNetCore_AutoLotDAL.EF;
+using DotNetCore_AutoLotDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Microsoft.EntityFrameworkCore.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore_AutoLotDAL.Repos
+{
+    public class InventoryRepo : BaseRepo<Inventory>, IInventoryRepo
+    {
+        public InventoryRepo(AutoLotContext context): base(context)
+        {
+        }
+
+        public override List<Inventory> GetAll() => GetAll(x => x.PetName, true).ToList();
+
+        public List<Inventory> GetPinkCars() => GetSome(x => x.Color == "Pink");
+
+        public List<Inventory> Search(string searchString) => 
+            Context.Cars.Where(x => Functions.Like(x.PetName, $"%{searchString}%")).ToList();
+
+        public List<Inventory> GetRelatedData() => 
+            Context.Cars.FromSql("SELECT * FROM Inventory").Include(x => x.Orders).ThenInclude(x => x.Customer).ToList();
+    }
+}
